a60e7cbbcc5d1666c8530e7f4a18037f171dbb8e
Robert Hardt <rob@Roberts-MBP.fios-router.home>
2017-11-13 12:40:44 -0500

    Add queries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,38 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+//var id = "5a09d4c8c7af95cf120e00bf11";
+//
+//if (!ObjectID.isValid(id)) {
+//	console.log('ID not valid');
+//}
+
+//
+//Todo.find({
+//	_id: id
+//}).then((todos) => {
+//	console.log('Todos', todos);
+//});
+//
+//Todo.findOne({
+//	_id: id
+//}).then((todo) => {
+//	console.log('Todo', todo);
+//});
+
+//Todo.findById(id).then((todo) => {
+//	if(!todo) {
+//		return console.log("ID not found");
+//	}
+//	console.log('Todo by ID', todo);
+//}).catch((e) => console.log(e));
+
+var userId = '5a0607a1c3e51d7b769c3ab9';
+User.findById(userId).then((user) => {
+	if(!user) {
+		return console.log("user ID not found");
+	}
+	console.log('User by ID', user);
+}).catch((e) => console.log(e));
